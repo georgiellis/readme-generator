96bd02c792cc8271b72bb051c6eeb6b2ca6608f2
Georgie <georginakellis@gmail.com>
2020-04-18 09:39:37 +0800

    Packages added, questions updated

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+var fs = require("fs");
+var inquirer = require("inquirer")
 
+inquirer
+.prompt ([
+    {
+        type:'input',
+        name: 'username',
+        message: 'What is your username?'
+    },
+    {
+        type:'input',
+        name: 'repository',
+        message: 'What is the repository name?',
+    }
+])
+.then(answers => {
+    // Use user feedback for... whatever!!
+})
+.catch(error => {
+    if(error.isTtyError){
+        // Promp couldn't be rendered in the current environment
+    } else {
+        //something else when wrong
+    }
+});
